303304ae5da8379afcc7720f4d662a9dda0aa1e5
Mouloudi Amine <dm_mouloudi@esi.dz>
2019-11-15 02:21:52 +0100

    mise en place Middleware logger

--- client/redux/store/index.js
+++ client/redux/store/index.js
@@ -1,4 +1,8 @@
-import { createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import rootReducer from "../reducers";
-const store = createStore(rootReducer);
+import {logger} from "../middleware/Middleware";
+const store = createStore(
+    rootReducer,
+    applyMiddleware(logger)
+);
 export default store;
